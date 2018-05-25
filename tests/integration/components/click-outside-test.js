23937ce33e331bb0cb9fbcd732d5f9981d457e23
Gabor Babicz <gabor.babicz@gmail.com>
2018-05-25 15:48:43 +0200

    Update tests

--- tests/integration/components/click-outside-test.js
+++ tests/integration/components/click-outside-test.js
@@ -1,83 +1,83 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { next } from '@ember/runloop';
-import { click } from 'ember-native-dom-helpers';
+import { render, click } from '@ember/test-helpers';
 
-moduleForComponent('click-outside', 'Integration | Component | click outside', {
-  integration: true
-});
+module('click-outside', 'Integration | Component | click outside', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('smoke test', function(assert) {
-  assert.expect(2);
+  test('smoke test', async function(assert) {
+    assert.expect(2);
 
-  this.on('didClickOutside', function(e) {
-    assert.ok('`didClickOutside` fired only once');
-    assert.equal(e.target.className, 'outside', 'the event object was passed and is correct');
+    this.set('didClickOutside', (e)=> {
+      assert.ok('`didClickOutside` fired only once');
+      assert.equal(e.target.className, 'outside', 'the event object was passed and is correct');
+    });
+
+    await render(hbs`
+      <div class="outside">Somewhere, over the rainbow...</div>
+
+      {{#click-outside action=(action didClickOutside)}}
+        <div class="inside">We're in</div>
+      {{/click-outside}}
+    `);
+
+    // It's important to fire the actions in the next run loop. Failing to do so
+    // would make the outside click not to fire. The reason for this is more
+    // often than not the component is rendered as a result of some user
+    // interaction, mainly a click. If the component attached the outside click
+    // event handler in the same loop, the handler would catch the event and send
+    // the action immediately.
+    await next(async ()=> {
+      await click('.inside');
+      await click('.outside');
+    });
   });
 
-  this.render(hbs`
-    <div class="outside">Somewhere, over the rainbow...</div>
+  test(`it doesn't throw without an action handler`, async function(assert) {
+    assert.expect(0);
 
-    {{#click-outside action=(action "didClickOutside")}}
-      <div class="inside">We're in</div>
-    {{/click-outside}}
-  `);
+    await render(hbs`
+      <div class="outside">Somewhere, over the rainbow...</div>
 
-  // It's important to fire the actions in the next run loop. Failing to do so
-  // would make the outside click not to fire. The reason for this is more
-  // often than not the component is rendered as a result of some user
-  // interaction, mainly a click. If the component attached the outside click
-  // event handler in the same loop, the handler would catch the event and send
-  // the action immediately.
-  next(()=> {
-    click('.inside');
-    click('.outside');
-  });
-});
+      {{#click-outside}}
+        <div class="inside">We're in</div>
+      {{/click-outside}}
+    `);
 
-test(`it doesn't throw without an action handler`, function(assert) {
-  assert.expect(0);
-
-  this.render(hbs`
-    <div class="outside">Somewhere, over the rainbow...</div>
-
-    {{#click-outside}}
-      <div class="inside">We're in</div>
-    {{/click-outside}}
-  `);
-
-  click('.outside');
-});
-
-test('except selector', function(assert) {
-  assert.expect(1);
-
-  this.on('didClickOutside', function() {
-    assert.ok('`didClickOutside` fired only once');
+    await click('.outside');
   });
 
-  this.render(hbs`
-    <div class="outside">Somewhere, over the rainbow...</div>
+  test('except selector', async function(assert) {
+    assert.expect(1);
 
-    <div class="except-outside">
-      <div>
-        Somewhere, under the rainbow...
+    this.set('didClickOutside', ()=> {
+      assert.ok('`didClickOutside` fired only once');
+    });
+
+    await render(hbs`
+      <div class="outside">Somewhere, over the rainbow...</div>
+
+      <div class="except-outside">
+        <div>
+          Somewhere, under the rainbow...
+        </div>
       </div>
-    </div>
 
-    {{#click-outside except-selector=".except-outside" action=(action "didClickOutside")}}
-    {{/click-outside}}
-  `);
+      {{#click-outside except-selector=".except-outside" action=(action didClickOutside)}}
+      {{/click-outside}}
+    `);
 
-  // It's important to fire the actions in the next run loop. Failing to do so
-  // would make the outside click not to fire. The reason for this is more
-  // often than not the component is rendered as a result of some user
-  // interaction, mainly a click. If the component attached the outside click
-  // event handler in the same loop, the handler would catch the event and send
-  // the action immediately.
-  next(()=> {
-    click('.outside');
-    click('.except-outside div');
+    // It's important to fire the actions in the next run loop. Failing to do so
+    // would make the outside click not to fire. The reason for this is more
+    // often than not the component is rendered as a result of some user
+    // interaction, mainly a click. If the component attached the outside click
+    // event handler in the same loop, the handler would catch the event and send
+    // the action immediately.
+    await next(async ()=> {
+      await click('.outside');
+      await click('.except-outside div');
+    });
   });
-
 });
