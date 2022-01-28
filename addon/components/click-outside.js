663502e10f7e9ca8a8729a93fb25900c926f4494
Gabor Babicz <gabor@gaborbabicz.com>
2022-01-28 12:25:39 +0100

    Remove deprecated exports

--- addon/components/click-outside.js
+++
@@ -1,8 +0,0 @@
-import Component from '../component';
-import { printConsoleMessage } from '../utils';
-
-printConsoleMessage(
-  `Importing 'ember-click-outside/components/click-outside' is deprecated. Please import from 'ember-click-outside/component' instead.`
-);
-
-export default Component;
